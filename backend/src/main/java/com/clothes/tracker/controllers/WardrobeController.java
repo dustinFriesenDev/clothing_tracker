ecc4ad3e4e8bcdc8ac6916af932ef87a40dd999c
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2024-03-09 11:37:32 -0500

    working on the put request for multiple checkmarks for multiple rows

--- backend/src/main/java/com/clothes/tracker/controllers/WardrobeController.java
+++ backend/src/main/java/com/clothes/tracker/controllers/WardrobeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.client.RestClientBuilderConfigurer;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,8 +24,9 @@ public class WardrobeController {
     }
 
 
+    //PATCH one item of clothing
     @PatchMapping("/update/{id}")
-    public Wardrobe updateWardrobe(@PathVariable int id, @RequestBody Wardrobe newWardrobe){
+    public Wardrobe updateSingleItem(@PathVariable int id, @RequestBody Wardrobe newWardrobe){
         return wardrobeRepository.findById(id)
                 .map(clothes -> {
 //                    clothes.setType(newWardrobe.getType());
@@ -37,5 +39,18 @@ public class WardrobeController {
                     return wardrobeRepository.save(clothes);
                 }).orElseThrow(() -> new Error("Item not found"));
     }
+
+    //PATCH HAVE IT for multiple items of clothing
+    @PutMapping("/update")
+    public Wardrobe updateWardrobe(@RequestBody List<Wardrobe> newWardrobe){
+        for (Wardrobe wardrobe : newWardrobe){
+            wardrobeRepository.updateItem(wardrobe.getId(), wardrobe.isHave());
+        }
+
+
+
+        List<Wardrobe> wardrobeItems = new ArrayList<>();
+        for (int i = 0)
+
 
 }
--- backend/src/main/java/com/clothes/tracker/data/WardrobeRepository.java
+++ backend/src/main/java/com/clothes/tracker/data/WardrobeRepository.java
@@ -2,9 +2,16 @@ package com.clothes.tracker.data;
 
 import com.clothes.tracker.model.Wardrobe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WardrobeRepository extends JpaRepository<Wardrobe, Integer> {
+
+    @Modifying
+    @Query("UPDATE Wardrobe u SET u.id = :id, u.haveIt = :haveIt")
+    void updateItem(@Param("id") int id, @Param("haveIt") Boolean haveIt);
 
 }
