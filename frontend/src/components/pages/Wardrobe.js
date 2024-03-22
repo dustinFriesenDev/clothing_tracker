202a7d1b4ca055008a989828293cc5c547944862
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2024-03-21 20:46:06 -0400

    updated put method. updating html to display other fields

--- frontend/src/components/pages/Wardrobe.js
+++ frontend/src/components/pages/Wardrobe.js
@@ -1,22 +1,34 @@
 import React, {useState, useEffect} from "react";
 import '../../index.css';
+import { type } from "@testing-library/user-event/dist/type";
 
 const Wardrobe = () => {
     const [id, setId] = useState("");
     const [data, setData] = useState([]);
     const [click, setClick] = useState("");
-    const [Have, setHave] = useState(false);
+    const [amount, setAmount] = useState();
+    const [color, setColor] = useState();
+    const [have, setHave] = useState(false);
+    const [season, setSeason] = useState();
+    const [size, setSize] = useState();
+    const [style, setStyle] = useState();
+    const [typeOfItem, setTypeOfItem] = useState();
     const urlWardrobe = "http://localhost:8080/wardrobe/all";
     const btn = document.querySelectorAll("button");
 
     //PUT method
     const iHaveThat = (e) => {
-        
         e.preventDefault();
         const finalFormEndpointUpdate = "http://localhost:8080/wardrobe/update/" + id;
         const updatedData = {
             //add other columns to keep information the same. add useState to set state for each variable.
-            "have" : Have
+            "amount" : amount,
+            "color" : color,
+            "have" : have,
+            "season" : season,
+            "size" : size,
+            "style" : style,
+            "type" : typeOfItem 
         };
 
         fetch(finalFormEndpointUpdate, {
@@ -50,7 +62,7 @@ const Wardrobe = () => {
 
     const checkHave = (e) => {
         if(click === e.target.id){
-            setHave(!Have);
+            setHave(!have);
         }
     }
 
@@ -67,8 +79,9 @@ const Wardrobe = () => {
                 <form key={clothing.id} onSubmit={iHaveThat} method="PATCH">
                 <div  className="clothing-description">
                     <div>
-                        <label id="verify">{`${clothing.style} ${clothing.type}`}</label>
-                        <input type="checkbox" id={clothing.id} checked={Have} onChange={checkHave} onClick={clicked} />
+                        <label value={clothing.type}>{`${clothing.style} ${clothing.type}`}</label>
+                        
+                        <input type="checkbox" id={clothing.id} checked={have} onChange={checkHave} onClick={clicked} />
                         <button type="submit">Save</button>
                     </div>
                 </div>
